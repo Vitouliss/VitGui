5b95e583905ff8dc332b2ac77ed8ec05c307afd2
Vitouliss14 <vitouliss14@yahoo.com>
2012-11-04 16:53:54 -0500

    Code for the UserInput library is added.

--- Library.lua
+++ Library.lua
@@ -18,7 +18,7 @@ local PopupTween = 0.400000006, 0, 0.300000012, 0
 
 
 _G.CreatePopup = function(titlecontent,parcontent)
-local main = Instance.new("ScreenGui",ScreenHolder)
+main = Instance.new("ScreenGui",ScreenHolder)
 main.Name = "Popup"
 
 frame = Instance.new("Frame",main) -- Because of scope. 
@@ -55,11 +55,41 @@ end
 --User input tttiimmeeeees
 
 _G.CreateUserInput = function()
+print("Library CreateUserInput called!") -- For debugging purposes. 
+
+local main2 = Instance.new("ScreenGui",ScreenHolder)
+main2.Name = "Holder"
+
+frame2 = Instance.new("Frame",main2)
+frame2.Name = "Background"
+frame2.BackgroundColor3 = Color3.new(0,0,0)
+frame2.BackgroundTransparency = 0.11
+frame2.Active = true
+frame2.Position = UDim2.new(PopupstartPos)
+frame2.Size = UDim2.new(0.300000012, 0, 0.300000012, 0)
+
+local titleinput = Instance.new("TextBox",frame2)
+titleinput.BackgroundColor3 = Color3.new(0,0,0)
+titleinput.Active = true
+titleinput.Size = UDim2.new(1, 0, 0.140000001, 0)
+titleinput.TextWrapped = true
+titleinput.Text = "TYPE SOMETHING HERE!"
+titleinput.MultiLine = true 
+titleinput.TextColor3 = Color3.new(255,255,255)
+
+local parinput = Instance.new("TextBox",frame2)
+parinput.BackgroundColor3 = Color3.new(0,0,0)
+parinput.BackgroundTransparency = 0
+parinput.Active = true
+parinput.MultiLine = true 
+parinput.Position = UDim2.new(0, 0, 0.159999996, 0)
+parinput.Size = UDim2.new(1, 0, 0.799999952, 0)
+parinput.Text = "TYPE SOMETHING HERE!"
+parinput.TextWrapped = true
+parinput.TextColor3 = Color3.new(255,255,255)
 
 end
 
 _G.CreateSlider = function()
 
 end
-
---Freaking test this thing or diiieeeeeee
